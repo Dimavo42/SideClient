5101160a37732724df31c0c7e20e37ecd5497933
Dima_Voronov <87165144+Dimavo42@users.noreply.github.com>
2023-06-01 22:28:52 +0300

    Update

---
+++ schemes/user.js
@@ -0,0 +1,15 @@
+const mongoes = require('mongoose');
+
+
+
+
+const userSchema = new Shema({
+        
+        “user_id”:123123,
+        “year”: 2023,
+        “month”:5,
+        “day”:12,
+        “description”:”milk”,
+        “category”:”food”,
+        “sum”:8
+});
