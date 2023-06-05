feebc4647e156c06c4e53fc05ad2a15d461b77fb
Dima_Voronov <87165144+Dimavo42@users.noreply.github.com>
2023-06-05 10:46:53 +0300

    Update Mongo

--- App.js
+++ App.js
@@ -5,6 +5,17 @@ const addcostRoutes = require('./routes/addcost');
 const reportRoutes = require('./routes/report');
 
 const app = express();
+const dbURI = 'mongodb://127.0.0.1';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+      console.log('Server started on port 3000');
+    });
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
 
 app.use('/addcost', addcostRoutes);
 app.use('/report', reportRoutes);
@@ -13,7 +24,6 @@ app.use('/about', aboutRoutes);
 
 app.use(express.json());
 app.use((err, req, res, next) => {
-    console.error(err);
     res.status(500).json({ error: 'Internal server error' });
   });
 
