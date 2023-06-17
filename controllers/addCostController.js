e51dbfa5fea8912c997ff4d48645f069713bd40b
Dima_Voronov <87165144+Dimavo42@users.noreply.github.com>
2023-06-17 13:24:36 +0300

    Update routes

--- controllers/addCostController.js
+++ controllers/addCostController.js
@@ -1,9 +1,8 @@
-const User = require('../schemas/userSchema');
-const Cost = require('../schemas/costSchema');
-const { availableCategories } = require('../utils/availableCategories');
+const User = require("../schemas/userSchema");
+const Cost = require("../schemas/costSchema");
+const { availableCategories } = require("../utils/availableCategories");
 
 const postAddCost = async (req, res) => {
-  console.log('addCost');
   // Check if all required parameters are provided
   const { user_id, year, month, day, description, category, sum } = req.body;
 
@@ -16,41 +15,43 @@ const postAddCost = async (req, res) => {
     !category ||
     !sum
   ) {
-    return res.status(400).json({ error: 'Missing parameters' });
+    return res.status(400).json({ error: "Missing parameters" });
   }
 
   // Check if the category is valid
   if (!availableCategories.includes(category)) {
-    return res.status(400).json({ error: 'Invalid category' });
+    return res.status(400).json({ error: "Invalid category" });
   }
 
   const user = await User.findOne({ id: user_id });
   // Check if is user is empty
   if (!user) {
-    return res.status(404).json({ error: 'Cannot find user' });
+    return res.status(404).json({ error: "Cannot find user" });
   }
-
-  const newCost = new Cost({
+  // Try to find one and add it sum
+  let existingCost = await Cost.findOne({
     user_id,
     year,
     month,
     day,
     description,
     category,
-    sum,
   });
-
-  // Save the new cost item to the database
-  newCost
-    .save()
-    .then(() => {
-      // Return the added cost item as the response
-      res.json(newCost);
-    })
-    .catch((err) => {
-      //Return error couldnt save
-      res.json({ error: `Could not save to DB: ${err.message}` });
-    });
+  if (existingCost) {
+    existingCost.sum += sum;
+    await existingCost.save();
+    res.json(existingCost);
+  } else {
+    // There is no cost with this parmaters then add newCost 
+    const newCost = new Cost({ user_id, year, month, day, description, category, sum });
+    newCost.save()
+      .then(() => {
+        res.json(newCost);
+      })
+      .catch((err) => {
+        res.status(400).json({ error: `Could not save to DB: ${err.message}` });
+      });
+  }
 };
 
 module.exports = { postAddCost };
--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,25 +1,24 @@
-const Report = require('../schemas/reportSchema');
-const User = require('../schemas/userSchema');
-const Cost = require('../schemas/costSchema');
+const Report = require("../schemas/reportSchema");
+const User = require("../schemas/userSchema");
+const Cost = require("../schemas/costSchema");
 
 const getReport = async (req, res) => {
-  console.log('report');
   const { user_id, month, year } = req.query;
 
   console.log(user_id, month, year);
 
   // Check if all required parameters are provided
   if (!user_id || !month || !year) {
-    return res.status(400).json({ error: 'Missing parameters' });
+    return res.status(400).json({ error: "Missing parameters" });
   }
   const user = await User.findOne({ id: user_id });
   // Check if is user is empty
   if (!user) {
-    return res.status(404).json({ error: 'Cannot find user' });
+    return res.status(404).json({ error: "Cannot find user" });
   }
   // Find a report if not
   let report = await Report.findOne({ user_id, month, year });
-  console.log('report', report);
+  const newCosts = await Cost.find({ user_id, month, year });
   if (!report) {
     // Create a new report if it doesn't exist
     report = new Report({
@@ -36,35 +35,26 @@ const getReport = async (req, res) => {
         other: [],
       },
     });
-
-    await report.save();
-  } else {
-    // If the report exists, return it without updating if there are no new costs
-    // const existingReport = report.categories;
-    const newCost = await Cost.find({ user_id, month, year });
-    //todo fix the compare
-    // if (existingReport.length === newCost.length) {
-    //   return res.json(report.categories);
-    // } else {
-    // Clear the existing category arrays in the report not equal have update
+  }
+  if (existingReport.length !== newCosts.length) {
+    // Clear the existing category arrays in the report
     Object.keys(report.categories).forEach((category) => {
       report.categories[category] = [];
     });
-    // Fill the arrays with new reports
-    newCost.forEach((cost) => {
+
+    // Fill the arrays with new costs
+    newCosts.forEach((cost) => {
       report.categories[cost.category].push({
         day: cost.day,
         description: cost.description,
         sum: cost.sum,
       });
     });
+
     // Save the report
     await report.save();
   }
-  // };
-
   res.json(report.categories);
-  //Send the report
 };
 
 module.exports = { getReport };
